fb4a6fa06c20ac6a3b23516e884e0f3902157933
VazquezG <vazquezguerra.oscar01@gmail.com>
2025-03-05 14:03:28 -0600

    Se agrego una variable de transform y se agrego la condicion para no pueda saltar multiples veces

--- TestingGame/Assets/PlayerMovement.cs
+++ TestingGame/Assets/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting;
+//using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,19 +6,26 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 8f;
     private Rigidbody2D rb;
+    Transform cubo;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cubo = GetComponent<Transform>();
     }
 
     void Update()
     {
        float moveX = Input.GetAxis("Horizontal");
        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        
+        if (Input.GetKeyDown(KeyCode.Space) && cubo.position.y < -2)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         } 
+
+        
     }
+
+    
 }
