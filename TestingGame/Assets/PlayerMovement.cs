1d3dafd7c5c4a5ee9f199238bdd0a2c7686b9c98
AndreBoi02 <a.puchie2@gmail.com>
2025-03-05 14:17:31 -0600

    Commit del equipo alfa buena maravilla onda dinamita escuadrón lobo, los cambios que se hicieron fueron: Cambio en el nivel de protección de las variables y serializaron para que se puedan modificar en Editor, se borró la librería que estaba en desuso, Se corrigió el salto infinito, creamos metodos para el movimiento y salto, estos llaman en el Update, se optimizó el código de salto y movimiento, para no crear Vectores nuevos inecesarios.

--- TestingGame/Assets/PlayerMovement.cs
+++ TestingGame/Assets/PlayerMovement.cs
@@ -1,24 +1,39 @@
-using Unity.VisualScripting;
+//using Unity.VisualScripting; Eliminar libreria que no se usa
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    public float speed = 5f;
-    public float jumpForce = 8f;
-    private Rigidbody2D rb;
-    void Start()
-    {
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField] float speed = 5f; //cambiamos el tipo de protección de las variables y agregamos el "SerializeField" para modificarla desde el editor
+    [SerializeField] float jumpForce = 8f; //cambiamos el tipo de protección de las variables y agregamos el "SerializeField" para modificarla desde el editor
+    Rigidbody2D rb;
+    [SerializeField] GameObject groundCheck;
+    bool isGrounded = false;
+
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-       float moveX = Input.GetAxis("Horizontal");
-       rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+    void Update() {
+        Move();
+        Jump();
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    void Move() {
+        float moveX = Input.GetAxis("Horizontal");
+        //rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+        rb.linearVelocityX = moveX * speed;
+    }
+
+    void Jump() {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            //rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            rb.linearVelocity = Vector2.up * jumpForce;
+            isGrounded = false;
         } 
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Ground")){
+            isGrounded = true;
+        }
     }
 }
