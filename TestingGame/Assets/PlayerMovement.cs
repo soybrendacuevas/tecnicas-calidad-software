d301ac79ff99d4bf4073e9cdeabc8168135d47e5
BlakesDV <dmunoz@cocoschool.mx>
2025-03-10 23:07:04 -0600

    PrimeraPracticaTestingTeam

--- TestingGame/Assets/PlayerMovement.cs
+++ TestingGame/Assets/PlayerMovement.cs
@@ -1,4 +1,5 @@
-using Unity.VisualScripting;
+//using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 8f;
     private Rigidbody2D rb;
+    private bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,9 +19,25 @@ public class PlayerMovement : MonoBehaviour
        float moveX = Input.GetAxis("Horizontal");
        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
         } 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
 }
